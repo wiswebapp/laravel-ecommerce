fb3572e7b0f9ac6f30db61d016bd38fb2d64850d
Tarang Biztech <tarang.p@biztechcs.com>
2022-08-16 18:14:18 +0530

    fix : title horizontal line

--- resources/views/admin/pages/pages_action.blade.php
+++ resources/views/admin/pages/pages_action.blade.php
@@ -16,8 +16,8 @@
     <section class="content-header">
       <div class="container-fluid">
         <div class="row mb-2">
-          <div class="col-sm-6">
-            <h1>{{$data['pageTitle']}}</h1>
+          <div class="col-sm-12">
+            <h1>{{$data['pageTitle']}}</h1><hr>
           </div>
         </div>
       </div>
